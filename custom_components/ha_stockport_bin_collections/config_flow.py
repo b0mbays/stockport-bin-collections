d4ed2b6eba383ca7775a3d534c5d3021de92d184
b0mbays <sam.s.lewis94@gmail.com>
2023-03-23 13:33:12 +0000

    Changes
    - Logo
    - Helpful info on setup
    - string json file

--- custom_components/ha_stockport_bin_collections/config_flow.py
+++ custom_components/ha_stockport_bin_collections/config_flow.py
@@ -12,10 +12,21 @@ class HaStockportBinCollectionsConfigFlow(config_entries.ConfigFlow, domain=DOMA
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="My Bin Collections", data=user_input)
+            unique_id = user_input["urn"]
+            await self.async_set_unique_id(unique_id)
+            self._abort_if_unique_id_configured()
 
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+            return self.async_create_entry(title="Stockport Bin Collection", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required("urn"): str}),
+            errors=errors,
+            description_placeholders={"base_url": "https://myaccount.stockport.gov.uk/address-finder/bin-collection"},
+        )
 
     @callback
     def async_get_options_flow(config_entry):
